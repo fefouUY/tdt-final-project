8d892a7fda2a7540a8729dde1f2c03217006ba69
fefouUY <fedeelvareliano@gmail.com>
2022-12-12 17:02:13 -0300

    second commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../assets/styles/navbar.css";
+
+export const Navbar = () => {
+    return (
+        <nav id="navbar">
+            <img
+                src="./assets/img/logo.png"
+                alt="logo"
+            />
+            <button>Salir</button>
+        </nav>
+    );
+};
+
+
